3e96803520f3cdf7a136f2258cfe16ea4b3102dc
Baturalp Yörük <baturalpyoruk1997@hotmail.com>
2020-04-10 13:02:44 +0300

    Add Spring Security

--- src/main/java/com/example/usertask/UserTaskApplication.java
+++ src/main/java/com/example/usertask/UserTaskApplication.java
@@ -1,12 +1,27 @@
 package com.example.usertask;
 
+import com.example.usertask.model.entity.ProcessEntity;
+import com.example.usertask.model.entity.TaskEntity;
+import com.example.usertask.model.entity.UserEntity;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication//(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+
+
+@SpringBootApplication
 public class UserTaskApplication {
 
 	public static void main(String[] args) {
+
+		SessionFactory sessionFactory =(SessionFactory) new Configuration().configure("hibernate.cfg.xml")
+				.addAnnotatedClass(UserEntity.class)
+				.addAnnotatedClass(TaskEntity.class)
+				.addAnnotatedClass(ProcessEntity.class).buildSessionFactory();
+		Session session = sessionFactory.getCurrentSession();
+
 		SpringApplication.run(UserTaskApplication.class, args);
 	}
 
