5cfb8d673fae535a4359b00406bae2b61ad0e320
Baturalp Yörük <baturalpyoruk1997@hotmail.com>
2020-04-22 17:55:48 +0300

    Add SearchUser, FindTasksMetrics to Controller

--- src/main/java/com/example/usertask/controller/UserController.java
+++ src/main/java/com/example/usertask/controller/UserController.java
@@ -2,8 +2,9 @@ package com.example.usertask.controller;
 
 import com.example.usertask.controller.request.CreateUserRequest;
 import com.example.usertask.controller.request.UpdateUserRequest;
+import com.example.usertask.model.dto.MetricDto;
+import com.example.usertask.model.dto.TaskDto;
 import com.example.usertask.model.dto.UserDto;
-import com.example.usertask.model.entity.UserEntity;
 import com.example.usertask.exception.UserNotFoundException;
 import com.example.usertask.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class UserController {
@@ -22,22 +24,15 @@ public class UserController {
         return userService.userList();
     }
 
-    /*@PostMapping("/users")
-    public UserDto createUser(@Valid @RequestBody UserDto userEntity) {
-        return userService.createUser(userEntity);
-    }*/
-
     @PostMapping("/users")
     public void createUser(@RequestBody CreateUserRequest request) {
         userService.createUser(request);
     }
 
-
     @GetMapping("/users/{id}")
     public UserDto getUserById(@PathVariable(value = "id") int id) throws UserNotFoundException {
         return userService.getUserById(id);
     }
-
 
     @PutMapping("/users/{id}")
     public UserDto updateUser(@PathVariable(value = "id") int id,
@@ -45,10 +40,19 @@ public class UserController {
         return userService.updateUser(id,updateUserRequest);
     }
 
-
     @DeleteMapping("/users/{id}")
     public void deleteUser(@PathVariable(value = "id") int id) throws UserNotFoundException {
         userService.deleteUser(id);
+    }
+
+    @GetMapping("/users/search/{keyword}")
+    public List<UserDto> getUserNameByName(@PathVariable(value = "keyword") String keyword){
+        return userService.getUserNameByName(keyword);
+    }
+
+    @GetMapping("/users/tasksMetrics/{id}")
+    public Map<TaskDto, List<MetricDto>> getTasksMetrics(@PathVariable(value = "id") int id) throws UserNotFoundException {
+        return userService.getTasksMetrics(id);
     }
 
 
